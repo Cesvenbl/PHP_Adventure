79641a60f07f0d64685897f21061eb1f0d397226
Cesvenbl <cedrik.svenblad@gymnasium.ax>
2017-01-13 09:57:22 +0200

    added br tags
    and fixed the centering of all content

--- index.php
+++ index.php
@@ -6,17 +6,16 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 </head>
-<div class="col-md-4 offset-md-4">
+<div class="col-md-5 offset-md-3">
 <h1>Da Quest For Weed</h1>
 <?php
 	//checking if we already have asked for a name and gotten a reply wia det $_GET parameter
 	if ($_GET['player_name'] == NULL):
 ?>
 <br>
-<br>
 <!--The form for asking after a name-->
 <form action="index.php">
-	<label>What tha fuck iz yo' name?</label>
+	<label>What tha fuck iz yo' name?</label><br>
 	<input type="text" name="player_name">
 	<input type="hidden" name="page" value="1">
 	<input type="submit" value="send">
@@ -29,6 +28,7 @@
 <h2>Hello <? echo $_GET[player_name];?></h2>
 <p>Yo ass is now standin up in tha middle of a gangbangin' forest.</p>
 <form action="index.php">
+	<br>
 	<label>In what direction do you go?</label>
 	<br>
 	<input type="radio" name="page" value="2">
@@ -50,19 +50,23 @@
 <?php
 	elseif ($_GET['page'] == 2):
 ?>
-<h2>Oh you went ta tha westside</h2>
-<p>i'm sorry ta rap but you fell tha fuck into tha handz of a mafia boss n' is now on yo' way ta tha slaughta house.</p>
+<br>
+<h2>Oh you went ta tha westside</h2><br>
+<p>i'm sorry ta rap but you fell tha fuck into tha handz of a mafia boss n' is now on yo' way ta tha slaughta house.</p><br>
 <?php
 	elseif ($_GET['page'] == 3):
 ?>
-<h2>oh you went to tha northside</h2>
+<br>
+<h2>oh you went to tha northside</h2><br>
 <p></p>
 <?php
 	elseif ($_GET['page'] == 4):
 ?>
+<br>
 <?php
 	elseif ($_GET['page'] == 5):
 ?>
+<br>
 <?php endif?>
 </div>
 </html>
