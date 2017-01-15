91d98b2341e4004b3ff6e9f0f989e1097dd44eb8
Cesvenbl <cedrik.svenblad@gymnasium.ax>
2017-01-15 12:11:57 +0200

    added spacing

--- index.php
+++ index.php
@@ -9,67 +9,85 @@
 </head><!--the begining of the visible things-->
 	<body background="img/Forest.jpg">
 		<div class="col-md-4 offset-md-4 whitememezman rounded">
-			<h1 class="text-center">Da Quest For Weed</h1>
-			<?php
-				//checking if we already have asked for a name and gotten a reply wia det $_GET parameter
-				if ($_GET['player_name'] == NULL):
-			?>
-			<br>
-			<!--The form for asking after a name-->
-				<form action="index.php">
-					<label>What tha fuck iz yo' name?</label><br>
-					<input type="text" name="player_name">
-					<input type="hidden" name="page" value="1">
-					<input type="submit" value="send">
-				</form>
-
+			<div class="text-center">
+				<h1>Da Quest For Weed</h1>
 				<?php
-					elseif ($_GET['page'] == 1):
+					//checking if we already have asked for a name and gotten a reply wia det $_GET parameter
+					if ($_GET['player_name'] == NULL):
 				?>
-			<!--The form that asks where the player wants to go-->
-				<p class="text-center">Hello <? echo $_GET[player_name];?></p>
-				<p class="text-center">Yo ass is now standin up in tha middle of a gangbangin' forest.</p>
+				<br>
+				<!--The form for asking after a name-->
 					<form action="index.php">
-						<br>
-						<label>In what direction do you go?</label>
-						<br>
-						<input type="radio" name="page" value="2">
-						<label for="west">Go to tha Westside</label>
-						<br>
-						<input type="radio" name="page" value="3">
-						<label for="north">Go to tha Northside</label>
-						<br>
-						<input type="radio" name="page" value="4">
-						<label for="east">Go to tha Eastside</label>
-						<br>
-						<input type="radio" name="page" value="5">
-						<label for="south">Go to tha Southside</label>
-						<input type="hidden" name="player_name" value="<?=$_GET[player_name]?>">
-						<br>
-						<input type="submit" value="Send">
+						<label>What tha fuck iz yo' name?</label><br>
+						<input type="text" name="player_name">
+						<input type="hidden" name="page" value="1">
+						<input type="submit" value="send">
 					</form>
-				<!--The Results of where the player wants to go-->
-				<?php
-					elseif ($_GET['page'] == 2):
-				?>
-				<br>
-				<h2 class="text-center">Oh you went ta tha westside</h2><br>
-				<p>i'm sorry ta rap but you fell tha fuck into tha handz of a mafia boss n' is now on yo' way ta tha slaughta house. Now You Dead</p>
-				<?php
-					elseif ($_GET['page'] == 3):
-				?>
-				<br>
-				<h2 class="text-center">oh you went to tha northside</h2><br>
-				<p></p>
-				<?php
-					elseif ($_GET['page'] == 4):
-				?>
-				<br>
-				<?php
-					elseif ($_GET['page'] == 5):
-				?>
-				<br>
-			<?php endif?>
+
+					<?php
+						elseif ($_GET['page'] == 1):
+					?>
+				<!--The form that asks where the player wants to go-->
+					<p>Hello <? echo $_GET[player_name];?></p>
+					<p>Yo ass is now standin up in tha middle of a gangbangin' forest.</p>
+						<form action="index.php">
+							<br>
+							<label>In what direction do you go?</label>
+							<br>
+							<input type="radio" name="page" value="2">
+							<label for="west">Go to tha Westside</label>
+							<br>
+							<input type="radio" name="page" value="3">
+							<label for="north">Go to tha Northside</label>
+							<br>
+							<input type="radio" name="page" value="4">
+							<label for="east">Go to tha Eastside</label>
+							<br>
+							<input type="radio" name="page" value="5">
+							<label for="south">Go to tha Southside</label>
+							<input type="hidden" name="player_name" value="<?=$_GET[player_name]?>">
+							<br>
+							<input type="submit" value="Send">
+						</form>
+					<!--The Results of where the player wants to go-->
+					<?php
+						elseif ($_GET['page'] == 2):
+					?>
+					<br>
+					<h2>Oh you went ta tha westside</h2><br>
+					<p>i'm sorry ta rap but you fell tha fuck into tha handz of a mafia boss n' is now on yo' way ta tha slaughta house. Now You Dead</p>
+					<?php
+						elseif ($_GET['page'] == 3):
+					?>
+					<br>
+					<h2>oh you went to tha northside</h2><br>
+					<p>this be tha wrong side of you hood no you be going back try to find da weed</p>
+					<input type="radio" name="page" value="1"><input type="submit" name="Okejs">
+					<?php
+						elseif ($_GET['page'] == 4):
+					?>
+					<h2>you went to tha eastside now you on tha right path</h2>
+					<p>this be tha right way it seems</p>
+					<form action="index.php">
+							<br>
+							<label>What box will you open?</label>
+							<br>
+							<input type="radio" name="box" value="1">
+							<label for="west">Go to tha Westside</label>
+							<br>
+							<input type="radio" name="page" value="2">
+							<label for="north">Go to tha Northside</label>
+							<input type="hidden" name="player_name" value="<?=$_GET[player_name]?>">
+							<br>
+							<input type="submit" value="Send">
+						</form>
+					<br>
+					<?php
+						elseif ($_GET['page'] == 5):
+					?>
+					<br>
+				<?php endif?>
+			</div>	
 		</div>
 	</body>
 </html>
